1af59ec274ab73c88e4900c4991bcc013877aad7
acarrillo <aecarrillo594@gmail.com>
2014-11-02 07:00:01 -0500

    Add non-blocking timeouts for commands

--- .willie/modules/twitchbot.py
+++ .willie/modules/twitchbot.py
@@ -38,81 +38,139 @@ GPIO.output(17, False)
 array = [11, 7, 8, 25]
 array2 =[27, 22, 10, 9]
 array3 =[2, 3, 4, 17]
+
+vip1_is_turning = False
+vip1_is_straight = False
+gat1_is_moving = False
+gat1_is_straight = False
+vip1_str8_time = 0
+vip2_str8_time = 0
+vip1_turn_time = 0
+vip2_turn_time = 0
+gat1_str8_time = 0
+gat2_str8_time = 0
+gat1_turn_time = 0
+gat2_turn_time = 0
+
 def vip1forward():
-        
-        GPIO.output(array[1], True)
-        GPIO.output(array[0], False)
-        GPIO.output(array[2], True)
-        GPIO.output(array[3], False)
+    global vip1_str8_time
+    global vip1_is_straight
+    vip1_str8_time = time.time()
+    vip1_is_straight = True
+    GPIO.output(array[1], True)
+    GPIO.output(array[0], False)
+    GPIO.output(array[2], True)
+    GPIO.output(array[3], False)
 
 
 def frog1forward():
-        GPIO.output(array3[1], False)
-        GPIO.output(array3[0], True)
-        GPIO.output(array3[2], False)
-        GPIO.output(array3[3], True)
+    GPIO.output(array3[1], False)
+    GPIO.output(array3[0], True)
+    GPIO.output(array3[2], False)
+    GPIO.output(array3[3], True)
 
 def frog1stop():
-        GPIO.output(array3[1], False)
-        GPIO.output(array3[0], False)
-        GPIO.output(array3[2], False)
-        GPIO.output(array3[3], False)        
+    GPIO.output(array3[1], False)
+    GPIO.output(array3[0], False)
+    GPIO.output(array3[2], False)
+    GPIO.output(array3[3], False)        
 
 def gat1counter():
-        GPIO.output(array2[1], False)
-        GPIO.output(array2[0], True)
-        GPIO.output(array2[2], True)
-        GPIO.output(array2[3], False)
+    global gat1_turn_time
+    global gat1_is_turning
+    gat1_is_turning = True
+    gat1_turn_time = time.time()
+    GPIO.output(array2[1], False)
+    GPIO.output(array2[0], True)
+    GPIO.output(array2[2], True)
+    GPIO.output(array2[3], False)
 
 def gat1reverse():
-        GPIO.output(array2[0], False)
-        GPIO.output(array2[1], True)
-        GPIO.output(array2[2], True)
-        GPIO.output(array2[3], False)
-
+    global gat1_str8_time
+    global gat1_is_straight
+    gat1_is_straight = True
+    gat1_str8_time = time.time()
+    GPIO.output(array2[0], False)
+    GPIO.output(array2[1], True)
+    GPIO.output(array2[2], True)
+    GPIO.output(array2[3], False)
 
 def gat1forward():
-        GPIO.output(array2[1], False)
-        GPIO.output(array2[0], True)
-        GPIO.output(array2[3], True)
-        GPIO.output(array2[2], False)
+    global gat1_str8_time
+    global gat1_is_straight
+    gat1_is_straight = True
+    gat1_str8_time = time.time()
+    GPIO.output(array2[1], False)
+    GPIO.output(array2[0], True)
+    GPIO.output(array2[3], True)
+    GPIO.output(array2[2], False)
         
 def vip1reverse():
-        GPIO.output(array[0], True)
-        GPIO.output(array[1], False)
-        GPIO.output(array[3], True)
-        GPIO.output(array[2], False)
+    global vip1_str8_time
+    global vip1_is_straight
+    vip1_is_straight = True
+    vip1_str8_time = time.time()
+    GPIO.output(array[0], True)
+    GPIO.output(array[1], False)
+    GPIO.output(array[3], True)
+    GPIO.output(array[2], False)
 
 def gat1clock():
-        GPIO.output(array2[1], True)
-        GPIO.output(array2[0], False)
-        GPIO.output(array2[2], False)
-        GPIO.output(array2[3], True) 
+    global gat1_turn_time
+    global gat1_is_turning
+    gat1_is_turning = True
+    gat1_turn_time = time.time()
+    GPIO.output(array2[1], True)
+    GPIO.output(array2[0], False)
+    GPIO.output(array2[2], False)
+    GPIO.output(array2[3], True) 
         
 
 def vip1stop():
-        GPIO.output(array[0], False)
-        GPIO.output(array[1], False)
-        GPIO.output(array[2], False)
-        GPIO.output(array[3], False)
+    vip1_is_turning = False
+    vip1_is_straight = False
+    GPIO.output(array[0], False)
+    GPIO.output(array[1], False)
+    GPIO.output(array[2], False)
+    GPIO.output(array[3], False)
 
 def gat1stop():
-        GPIO.output(array2[0], False)
-        GPIO.output(array2[1], False)
-        GPIO.output(array2[2], False)
-        GPIO.output(array2[3], False)
+    gat1_is_turning = False
+    gat1_is_straight = False
+    GPIO.output(array2[0], False)
+    GPIO.output(array2[1], False)
+    GPIO.output(array2[2], False)
+    GPIO.output(array2[3], False)
         
 def vip1clock():
-        GPIO.output(array[1], True)
-        GPIO.output(array[0], False)
-        GPIO.output(array[3], True)
-        GPIO.output(array[2], False)
+    vip1_turn_time = time.time()
+    GPIO.output(array[1], True)
+    GPIO.output(array[0], False)
+    GPIO.output(array[3], True)
+    GPIO.output(array[2], False)
 
 def vip1counter():
-        GPIO.output(array[0], True)
-        GPIO.output(array[1], False)
-        GPIO.output(array[2], True)
-        GPIO.output(array[3], False)        
+    vip1_turn_time = time.time()
+    GPIO.output(array[0], True)
+    GPIO.output(array[1], False)
+    GPIO.output(array[2], True)
+    GPIO.output(array[3], False)        
+
+
+@willie.module.interval(0.375)
+def checkTimeouts(bot):
+    forconst = 0.5
+    turnconst = 2.5
+
+    if vip1_is_turning and time.time() - vip1_turn_time > turnconst:
+        vip1stop()
+    elif vip1_is_straight and time.time() - vip1_str8_time > forconst:
+        vip1stop()
+    elif gat1_is_turning and time.time() - gat1_turn_time > turnconst:
+        gat1stop()
+    elif gat1_is_straight and time.time() - gat1_str8_time > forconst:
+        gat1stop()
+
 
 
 @willie.module.rule('([^\s]+)')
@@ -122,38 +180,22 @@ def helloworld(bot, trigger):
     if cmd == "for1":
         bot.say("asdfExecuting " + cmd)
         vip1forward()
-        time.sleep(1)
-        vip1stop()
     elif cmd == "rev1":
         bot.say("Executing " + cmd)
         vip1reverse()
-        time.sleep(1)
-        vip1stop()
     elif cmd == "clock1":
         bot.say("Executing " + cmd)
         vip1clock()
-        time.sleep(1.75)
-        vip1stop()
     elif cmd == "count1":
         bot.say("Executing " + cmd)
         vip1counter()
-        time.sleep(1.75)
-        vip1stop()
     elif cmd == "for2":
         bot.say("Executing " + cmd)
         gat1forward()
-        time.sleep(1)
-        gat1stop()
     elif cmd == "rev2":
         bot.say("Executing " + cmd)
         gat1reverse()
-        time.sleep(1)
-        gat1stop()
     elif cmd == "clock2":
         gat1clock()
-        time.sleep(1.75)
-        gat1stop()
     elif cmd == "count2":
         gat1counter()
-        time.sleep(1.75)
-        gat1stop()
